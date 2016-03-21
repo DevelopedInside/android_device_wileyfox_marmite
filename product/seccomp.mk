319a8f8c68ba70841e5789fe29a2d76b932561f6
Praveen Chavan <pchavan@codeaurora.org>
2016-03-21 09:37:14 -0700

    marmite: mediacodec: Allow select and eventfd syscalls
    Add pselect6 and eventfd2 syscalls
    
    Bug: 27654762
    
    Change-Id: I9aba945e50bece5b94f4fd15e44bc84d50a41a0b

--- product/seccomp.mk
+++ product/seccomp.mk
@@ -1,3 +1,4 @@
 # Seccomp
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/seccomp/mediaextractor-seccomp.policy:system/vendor/etc/seccomp_policy/mediaextractor.policy
+    $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/seccomp/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
