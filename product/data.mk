c367d565a8fa50278633f558b2146948828f0c6f
BeYkeRYkt <beykerykt@gmail.com>
2017-06-16 03:56:38 +0900

    marmite: move data files to device

---
+++ product/data.mk
@@ -0,0 +1,5 @@
+# Data
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/data/dsi_config.xml:system/etc/data/dsi_config.xml \
+    $(LOCAL_PATH)/configs/data/netmgr_config.xml:system/etc/data/netmgr_config.xml \
+    $(LOCAL_PATH)/configs/data/qmi_config.xml:system/etc/data/qmi_config.xml
