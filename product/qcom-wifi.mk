4966222cb2043ed8070b72da51e842f0f207da82
BeYkeRYkt <beykerykt@gmail.com>
2017-06-18 01:27:43 +0900

    marmite: add wifi prima binaries from caf
    * https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/wlan/prima/tree/?h=LA.UM.5.3.1_rb1

---
+++ product/qcom-wifi.mk
@@ -0,0 +1,9 @@
+# WCNSS firmware (taked from CAF-wlan)
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
+    $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
+    $(LOCAL_PATH)/wifi/WCNSS_wlan_dictionary.dat:system/etc/firmware/wlan/prima/WCNSS_wlan_dictionary.dat \
+    $(LOCAL_PATH)/wifi/WCNSS_qcom_wlan_nv.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin
+
+PRODUCT_PACKAGES += \
+    wcnss_service
