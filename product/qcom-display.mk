e7f97d6f3ae4848b05eea573c048857d7a5bfa49
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:03:01 +0200

    marmite: Update display/render for O
    * Add various HALs
    * Enable HWC2 & Gralloc1
    
    Change-Id: I98a86dc32edfc4c089bfda2234e4d27c309b56a8

--- product/qcom-display.mk
+++ product/qcom-display.mk
@@ -1,13 +1,23 @@
 # Display
 PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.memtrack@1.0-impl \
     gralloc.msm8937 \
     copybit.msm8937 \
     hwcomposer.msm8937 \
     memtrack.msm8937 \
+    libgenlock \
     liboverlay \
-    libtinyxml 
+    libtinyxml
 
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:system/etc/permissions/android.hardware.opengles.aep.xml
- 
+
+
+# RenderScript HAL
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0-impl
