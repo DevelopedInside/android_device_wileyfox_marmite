b2bcffb224265f19971373dd7234fd758c8c1d3e
nicknitewolf <nicknitewolf@lineageos.org>
2017-08-30 17:00:16 +0800

    marmite: Change WCNSS_qcom_cfg.ini file path
    Change-Id: I48381daba9c839379302e638cc8a3d6388563f26

--- product/qcom-wifi.mk
+++ product/qcom-wifi.mk
@@ -1,8 +1,8 @@
 # WCNSS firmware (taked from CAF-wlan)
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/wifi/fstman.ini:system/etc/wifi/fstman.ini \
-    $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
-    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini
+    $(LOCAL_PATH)/wifi/fstman.ini:system/vendor/etc/wifi/fstman.ini \
+    $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/vendor/etc/firmware/wlan/prima/WCNSS_cfg.dat \
+    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/vendor/etc/wifi/WCNSS_qcom_cfg.ini
 
 PRODUCT_PACKAGES += \
     wcnss_service \
