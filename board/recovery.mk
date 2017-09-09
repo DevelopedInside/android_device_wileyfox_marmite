e8fef9dcf1f45ff1193cd957df3b3fc8af7fc17f
Rashed Abdel-Tawab <rashed@linux.com>
2017-09-08 17:48:17 -0700

    marmite: Update for early-mounted system partition
     * This removes /system entry from fstab, since this partition
       is now early-mounted and the fstab entry is specified in device tree.
    
     * At the same time, also include a fully populated fstab, in order to avoid
       build breakage while generating recovery updater scripts.
    
    Change-Id: I8e558458b984e97a45a920c3751a6b7c3f2ebc5c

--- board/recovery.mk
+++ board/recovery.mk
@@ -1,3 +1,3 @@
 # Recovery
-TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/root/fstab.qcom
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/root/fstab.full
 TARGET_RECOVERY_UPDATER_LIBS := librecovery_updater_marmite
