dff5793df66527e99d179eb0b0ff7f9fbd9122d7
Vladimir Mikhaylov <beykerykt@gmail.com>
2017-09-14 22:52:38 +0700

    marmite: bt sync with caf

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -19,13 +19,20 @@
 #define _BDROID_BUILDCFG_H
 
 #define BTM_DEF_LOCAL_NAME   "Swift 2"
+#define BLUETOOTH_QTI_SW TRUE
 
+// Disables read remote device feature
+#define BTA_SKIP_BLE_READ_REMOTE_FEAT FALSE
 #define MAX_ACL_CONNECTIONS    7
 #define MAX_L2CAP_CHANNELS    16
 #define BLE_VND_INCLUDED   TRUE
-#define BLE_PERIPHERAL_ADV_NAME  FALSE
-#define BTA_SKIP_BLE_READ_REMOTE_FEAT FALSE
+
+// skips conn update at conn completion
 #define BTA_BLE_SKIP_CONN_UPD  FALSE
-#define BTM_LE_SECURE_CONN  TRUE
+#define BLE_PERIPHERAL_ADV_NAME  FALSE
 #define BT_CLEAN_TURN_ON_DISABLED 1
+
+#define BTA_DISABLE_DELAY 1000 /* in milliseconds */
+#define BTM_WBS_INCLUDED TRUE
+#define BTIF_HF_WBS_PREFERRED TRUE
 #endif
