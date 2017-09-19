48aae52d257733e6fd742960787748e15e363109
BeYkeRYkt <beykerykt@gmail.com>
2017-09-19 22:14:58 +0900

    marmite: update board *.mk

--- board/dexopt.mk
+++ board/dexopt.mk
@@ -6,3 +6,4 @@ ifeq ($(HOST_OS),linux)
     endif
   endif
 endif
+WITH_DEXPREOPT_BOOT_IMG_ONLY ?= true
--- board/kernel.mk
+++ board/kernel.mk
@@ -1,6 +1,4 @@
 # Kernel
-BOARD_DTBTOOL_ARGS := -2
-
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78B0000
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80000000
