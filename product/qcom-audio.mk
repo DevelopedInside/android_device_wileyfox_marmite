e80482fd2c30330718d6d58b48bcba80f877200e
BeYkeRYkt <beykerykt@gmail.com>
2017-10-08 22:59:52 +0700

    marmite: audio: remove duplicate properties

--- product/qcom-audio.mk
+++ product/qcom-audio.mk
@@ -74,8 +74,3 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.midi.xml:system/etc/permissions/android.software.midi.xml \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml
-
-# Properties
-PRODUCT_PROPERTY_OVERRIDES += \
-    af.fast_track_multiplier=1 \
-    audio_hal.period_size=192
