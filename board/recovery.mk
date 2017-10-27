27ed7f793eb16cfad4bd8024037e43b3aed58463
BeYkeRYkt <beykerykt@gmail.com>
2017-10-27 16:16:38 +0900

    marmite: add bootloader assert scripts
    * Stock Android 7.1.2 blobs require new firmware.
    * Scripts taked from op3 and hammerheadcaf
    * Perhaps in the future we will 'assert on modem build time-stamp'
    * Fix this commit: 2abe885ae1cb41558a85f5823d6e9188c262a218
    
    Change-Id: Ie8c245c6b92578c4131da91fd6176053735b0900

--- board/recovery.mk
+++ board/recovery.mk
@@ -1,2 +1,3 @@
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/root/fstab.qcom
+TARGET_RECOVERY_UPDATER_LIBS := librecovery_updater_marmite
---
+++ recovery/Android.mk
@@ -0,0 +1,13 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+ifneq ($(TARGET_PLATFORM_DEVICE_BASE),)
+LOCAL_CFLAGS += -DUSES_BOOTDEVICE_PATH
+endif
+
+LOCAL_C_INCLUDES := $(call project-path-for,recovery)
+LOCAL_SRC_FILES := recovery_updater.cpp
+LOCAL_MODULE := librecovery_updater_marmite
+LOCAL_MODULE_TAGS := eng
+include $(BUILD_STATIC_LIBRARY)
