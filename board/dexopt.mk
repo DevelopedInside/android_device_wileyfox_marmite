4649d103027724100af85550f25eb410b5d65f72
vm03 <vasy@vasy.ru>
2017-11-21 13:25:40 +0300

    marmite: Enable dexpreopt for user and userdebug builds

--- board/dexopt.mk
+++ board/dexopt.mk
@@ -1,6 +1,6 @@
 # Enable dexpreopt to speed boot time
 ifeq ($(HOST_OS),linux)
-  ifeq ($(call match-word-in-list,$(TARGET_BUILD_VARIANT),user),true)
+  ifneq ($(TARGET_BUILD_VARIANT),eng)
     ifeq ($(WITH_DEXPREOPT),)
       WITH_DEXPREOPT := true
     endif
