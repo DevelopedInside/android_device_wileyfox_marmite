6a935e93283314755f6e99453c1a4294f6d182ad
Artem Borisov <dedsa2002@gmail.com>
2017-12-22 11:45:58 +0300

    marmite: Dex-preopt boot and system server
    WITH_DEXPREOPT_BOOT_IMG_ONLY was removed in 8.1 in favor of
    WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY.
    
    Along with boot classpath it will also pre-optimize system server jars.
    
    Change-Id: Idd0ac1ac39a6c3d14637a974a0a99953c99af236

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -124,12 +124,10 @@ BOARD_USES_QCNE := true
 # Enable dexpreopt to speed boot time
 ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-    endif
+    WITH_DEXPREOPT ?= true
   endif
 endif
-WITH_DEXPREOPT_BOOT_IMG_ONLY ?= true
+WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY ?= true
 
 # DRM
 TARGET_ENABLE_MEDIADRM_64 := true
