264b9015caa1e03b67ac736850323126c2f17bd0
LuK1337 <priv.luk@gmail.com>
2017-12-31 15:25:59 +0100

    marmite: Remove AOSP seccomp policy
    While it served the purpose so far, no longer works when using
    CAF media HAL. Remove it to allow clean picks from CAf.
    
    This reverts commit 92ebc5baeb74daaaf80f05f97e48a4f0d5e42f06.
    
    Change-Id: I50edd601c24b40b735dd167435ae4a1d77ea76ad

--- product/seccomp.mk
+++ product/seccomp.mk
@@ -1,4 +1,3 @@
 # Seccomp
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:system/vendor/etc/seccomp_policy/mediacodec.policy \
     $(LOCAL_PATH)/seccomp/mediaextractor-seccomp.policy:system/vendor/etc/seccomp_policy/mediaextractor.policy
