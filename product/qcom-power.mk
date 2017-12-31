45571cd34dfb1c85f48678a8a64744b47139e06d
Rashed Abdel-Tawab <rashed@linux.com>
2017-12-31 04:40:40 +0200

    marmite: Build QTI HIDL power 1.0 HAL

--- product/qcom-power.mk
+++ product/qcom-power.mk
@@ -2,12 +2,18 @@
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/msm_irqbalance.conf:system/vendor/etc/msm_irqbalance.conf
 
-# Power HAL
-PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-impl \
-    android.hardware.power@1.0-service \
-    power.msm8937
-
 # QCOM
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
+
+# Power HAL
+PRODUCT_PACKAGES += \
+    android.hardware.power@1.0-service-qti
+
+# Powerhint
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+
+# Public Libraries
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
