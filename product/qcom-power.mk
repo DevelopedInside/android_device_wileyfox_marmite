dffd0c537aaf486935cab1c9f427b3bb0475d322
Rashed Abdel-Tawab <rashed@linux.com>
2018-01-07 13:53:04 -0800

    marmite: Add QTI low power whitelist
    Change-Id: Idb4a954979f916a7903ead982bf18b33e5cf1baf

--- product/qcom-power.mk
+++ product/qcom-power.mk
@@ -7,3 +7,7 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.0-impl \
     android.hardware.power@1.0-service \
     power.msm8937
+
+# QCOM
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
