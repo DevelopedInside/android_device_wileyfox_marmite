6a3bc167e9da22afad0ac404c157419ced40cc27
Bruno Martins <bgcngm@gmail.com>
2018-01-08 00:06:46 +0000

    marmite: Update GPS stack to O
     * From tissot 8.0.0-OPR1.170623.026-7.12.29
    
    Change-Id: Id4f7249149ed6fc192c89cc44eef4c115abc7a05

--- product/qcom-gps.mk
+++ product/qcom-gps.mk
@@ -1,16 +1,18 @@
 # GPS
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/gps/flp.conf:system/etc/flp.conf \
-    $(LOCAL_PATH)/configs/gps/gps.conf:system/etc/gps.conf \
-    $(LOCAL_PATH)/configs/gps/izat.conf:system/etc/izat.conf \
-    $(LOCAL_PATH)/configs/gps/lowi.conf:system/etc/lowi.conf \
-    $(LOCAL_PATH)/configs/gps/sap.conf:system/etc/sap.conf \
-    $(LOCAL_PATH)/configs/gps/xtwifi.conf:system/etc/xtwifi.conf
+    $(LOCAL_PATH)/configs/gps/apdr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/apdr.conf \
+    $(LOCAL_PATH)/configs/gps/flp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/flp.conf \
+    $(LOCAL_PATH)/configs/gps/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf \
+    $(LOCAL_PATH)/configs/gps/izat.conf:$(TARGET_COPY_OUT_VENDOR)/etc/izat.conf \
+    $(LOCAL_PATH)/configs/gps/lowi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/lowi.conf \
+    $(LOCAL_PATH)/configs/gps/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
+    $(LOCAL_PATH)/configs/gps/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf
 
 # GPS
 PRODUCT_PACKAGES += \
-    gps.msm8937 \
+    android.hardware.gnss@1.0-impl-qti \
     libcurl \
+    libgnss \
     libgnsspps
 
 # Permissions
