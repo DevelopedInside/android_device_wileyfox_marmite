88cec86440247c12c00931cba46d30f45a5230bf
Rashed Abdel-Tawab <rashed@linux.com>
2018-01-08 19:21:59 -0800

    marmite: Build RCS and telephony-ext
    Change-Id: I456aa2db554647f4f6303964f7fb071f98b923ac

---
+++ product/qcom-telephony-ext.mk
@@ -0,0 +1,13 @@
+# RCS
+PRODUCT_PACKAGES += \
+    rcs_service_aidl \
+    rcs_service_aidl.xml \
+    rcs_service_api \
+    rcs_service_api.xml
+
+# Telephony
+PRODUCT_PACKAGES += \
+    telephony-ext
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
