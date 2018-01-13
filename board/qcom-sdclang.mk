9039aea85ec543a76abd4e701206cfebeb435300
BeYkeRYkt <beykerykt@gmail.com>
2018-01-14 06:40:05 +0900

    marmite: fix missing space
    Change-Id: Iccd1e93f427addb74131eca6fa9b7aa813dab636

--- board/qcom-sdclang.mk
+++ board/qcom-sdclang.mk
@@ -2,7 +2,7 @@
 TARGET_USE_SDCLANG := false
 
 # SDClang 3.8.8
-ifeq($(TARGET_USE_SDCLANG),true)
+ifeq ($(TARGET_USE_SDCLANG),true)
 ifneq ($(HOST_OS),darwin)
 SDCLANG := true
 SDCLANG_PATH := prebuilts/clang/linux-x86/host/sdclang-3.8.8/bin
