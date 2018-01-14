4d70cf3a1328c056ba4a0294d939cc12d905f78b
BeYkeRYkt <beykerykt@gmail.com>
2018-01-15 01:54:14 +0900

    marmite: rebase libinit
    * based on msm8916-common
    * move hwui to libinit
    * formatted
    
    Change-Id: Ieb82971928dd07a7c5270472fbfa1974304134c8

--- init/Android.mk
+++ init/Android.mk
@@ -18,9 +18,12 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_C_INCLUDES := system/core/init
-LOCAL_MODULE := libinit_marmite
 LOCAL_MODULE_TAGS := optional
+LOCAL_C_INCLUDES := \
+    system/core/base/include \
+    system/core/init
+LOCAL_CPPFLAGS := -Wall
 LOCAL_SRC_FILES := init_marmite.cpp
+LOCAL_MODULE := libinit_marmite
 
 include $(BUILD_STATIC_LIBRARY)
--- init/init_marmite.cpp
+++ init/init_marmite.cpp
@@ -28,92 +28,90 @@
  */
 
 #include <cstdlib>
-#include <string>
+#include <fstream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/sysinfo.h>
 
-#include <unistd.h>
-#include <fcntl.h>
 #include <dirent.h>
 #include <errno.h>
 
-#include <sys/types.h>
-#include <sys/sysinfo.h>
+#include <android-base/strings.h>
 
 #include "vendor_init.h"
 #include "property_service.h"
 #include "log.h"
 #include "util.h"
 
-char const *heapstartsize;
-char const *heapgrowthlimit;
-char const *heapsize;
-char const *heapminfree;
+using android::base::Trim;
 
-static int read_file2(const char *fname, char *data, int max_size) {
-  int fd, rc;
+char const* heapstartsize;
+char const* heapgrowthlimit;
+char const* heapsize;
+char const* heapminfree;
+char const* heapmaxfree;
+char const* large_cache_height;
 
-  if (max_size < 1) return 0;
+static void init_alarm_boot_properties()
+{
+    char const* boot_reason_file = "/proc/sys/kernel/boot_reason";
+    char const* power_off_alarm_file = "/persist/alarm/powerOffAlarmSet";
+    std::string boot_reason;
+    std::string power_off_alarm;
+    std::string tmp = property_get("ro.boot.alarmboot");
 
-  fd = open(fname, O_RDONLY);
-  if (fd < 0) {
-    ERROR("failed to open '%s'\n", fname);
-    return 0;
-  }
-
-  rc = read(fd, data, max_size - 1);
-  if ((rc > 0) && (rc < max_size))
-    data[rc] = '\0';
-  else
-    data[0] = '\0';
-  close(fd);
-
-  return 1;
+    if (read_file(boot_reason_file, &boot_reason)
+        && read_file(power_off_alarm_file, &power_off_alarm))
+    {
+        /*
+         * Setup ro.alarm_boot value to true when it is RTC triggered boot up
+         * For existing PMIC chips, the following mapping applies
+         * for the value of boot_reason:
+         *
+         * 0 -> unknown
+         * 1 -> hard reset
+         * 2 -> sudden momentary power loss (SMPL)
+         * 3 -> real time clock (RTC)
+         * 4 -> DC charger inserted
+         * 5 -> USB charger insertd
+         * 6 -> PON1 pin toggled (for secondary PMICs)
+         * 7 -> CBLPWR_N pin toggled (for external power supply)
+         * 8 -> KPDPWR_N pin toggled (power key pressed)
+         */
+        if ((Trim(boot_reason) == "3" || tmp == "true") && Trim(power_off_alarm) == "1")
+            property_set("ro.alarm_boot", "true");
+        else
+            property_set("ro.alarm_boot", "false");
+    }
 }
 
-static void init_alarm_boot_properties() {
-  char const *alarm_file = "/proc/sys/kernel/boot_reason";
-  char buf[64];
+void check_device()
+{
+    struct sysinfo sys;
 
-  if (read_file2(alarm_file, buf, sizeof(buf))) {
-    /*
-     * Setup ro.alarm_boot value to true when it is RTC triggered boot up
-     * For existing PMIC chips, the following mapping applies
-     * for the value of boot_reason:
-     *
-     * 0 -> unknown
-     * 1 -> hard reset
-     * 2 -> sudden momentary power loss (SMPL)
-     * 3 -> real time clock (RTC)
-     * 4 -> DC charger inserted
-     * 5 -> USB charger insertd
-     * 6 -> PON1 pin toggled (for secondary PMICs)
-     * 7 -> CBLPWR_N pin toggled (for external power supply)
-     * 8 -> KPDPWR_N pin toggled (power key pressed)
-     */
-    if (buf[0] == '3')
-      property_set("ro.alarm_boot", "true");
+    sysinfo(&sys);
+
+    if (sys.totalram > 2048ull * 1024 * 1024)
+    {
+        // from - phone-xxhdpi-3072-dalvik-heap.mk
+        heapstartsize = "8m";
+        heapgrowthlimit = "288m";
+        heapsize = "768m";
+        heapminfree = "512k";
+        heapmaxfree = "8m";
+        large_cache_height = "1024";
+    }
     else
-      property_set("ro.alarm_boot", "false");
-  }
-}
-
-void check_device() {
-  struct sysinfo sys;
-
-  sysinfo(&sys);
-
-  if (sys.totalram > 2048ull * 1024 * 1024) {
-    // from - phone-xxhdpi-3072-dalvik-heap.mk
-    heapstartsize = "8m";
-    heapgrowthlimit = "288m";
-    heapsize = "768m";
-    heapminfree = "2m";
-  } else {
-    // from - phone-xxhdpi-2048-dalvik-heap.mk
-    heapstartsize = "16m";
-    heapgrowthlimit = "192m";
-    heapsize = "512m";
-    heapminfree = "2m";
-  }
+    {
+        // from - phone-xxhdpi-2048-dalvik-heap.mk
+        heapstartsize = "16m";
+        heapgrowthlimit = "192m";
+        heapsize = "512m";
+        heapminfree = "2m";
+        heapmaxfree = "8m";
+        large_cache_height = "1024";
+    }
 }
 
 /*
@@ -127,61 +125,74 @@ void check_device() {
  *
  * @Author BeYkeRYkt (21-09-2017)
  */
-void check_aw87319() {
-  DIR *dir = opendir("/sys/bus/i2c/drivers/AW87319_PA/2-0058");
-  if (dir) {
-    /* Directory exists. */
-    // Based on:
-    // https://github.com/CyanogenMod/android_hardware_qcom_audio/commit/f6cfe88a8959aacbb0d1782265d4fba52c8854da
-    property_set("ro.audio.customplatform", "AW87319");
-    property_set("audio.acdb.name", "AW87319");
-    property_set("persist.audio.calfile0",
-                 "/etc/acdbdata/AW87319/AW87319_Bluetooth_cal.acdb");
-    property_set("persist.audio.calfile1",
-                 "/etc/acdbdata/AW87319/AW87319_General_cal.acdb");
-    property_set("persist.audio.calfile2",
-                 "/etc/acdbdata/AW87319/AW87319_Global_cal.acdb");
-    property_set("persist.audio.calfile3",
-                 "/etc/acdbdata/AW87319/AW87319_Handset_cal.acdb");
-    property_set("persist.audio.calfile4",
-                 "/etc/acdbdata/AW87319/AW87319_Hdmi_cal.acdb");
-    property_set("persist.audio.calfile5",
-                 "/etc/acdbdata/AW87319/AW87319_Headset_cal.acdb");
-    property_set("persist.audio.calfile6",
-                 "/etc/acdbdata/AW87319/AW87319_Speaker_cal.acdb");
-    closedir(dir);
-  }
+void check_aw87319()
+{
+    DIR* dir = opendir("/sys/bus/i2c/drivers/AW87319_PA/2-0058");
+    if (dir)
+    {
+        /* Directory exists. */
+        // Based on:
+        // https://github.com/CyanogenMod/android_hardware_qcom_audio/commit/f6cfe88a8959aacbb0d1782265d4fba52c8854da
+        property_set("ro.audio.customplatform", "AW87319");
+        property_set("audio.acdb.name", "AW87319");
+        property_set("persist.audio.calfile0", "/etc/acdbdata/AW87319/AW87319_Bluetooth_cal.acdb");
+        property_set("persist.audio.calfile1", "/etc/acdbdata/AW87319/AW87319_General_cal.acdb");
+        property_set("persist.audio.calfile2", "/etc/acdbdata/AW87319/AW87319_Global_cal.acdb");
+        property_set("persist.audio.calfile3", "/etc/acdbdata/AW87319/AW87319_Handset_cal.acdb");
+        property_set("persist.audio.calfile4", "/etc/acdbdata/AW87319/AW87319_Hdmi_cal.acdb");
+        property_set("persist.audio.calfile5", "/etc/acdbdata/AW87319/AW87319_Headset_cal.acdb");
+        property_set("persist.audio.calfile6", "/etc/acdbdata/AW87319/AW87319_Speaker_cal.acdb");
+        closedir(dir);
+    }
 }
 
-void vendor_load_properties() {
-  check_device();
-  check_aw87319();
+void vendor_load_properties()
+{
+    init_alarm_boot_properties();
 
-  property_set("dalvik.vm.heapstartsize", heapstartsize);
-  property_set("dalvik.vm.heapgrowthlimit", heapgrowthlimit);
-  property_set("dalvik.vm.heapsize", heapsize);
-  property_set("dalvik.vm.heaptargetutilization", "0.75");
-  property_set("dalvik.vm.heapminfree", heapminfree);
-  property_set("dalvik.vm.heapmaxfree", "8m");
+    check_device();
+    property_set("dalvik.vm.heapstartsize", heapstartsize);
+    property_set("dalvik.vm.heapgrowthlimit", heapgrowthlimit);
+    property_set("dalvik.vm.heapsize", heapsize);
+    property_set("dalvik.vm.heaptargetutilization", "0.75");
+    property_set("dalvik.vm.heapminfree", heapminfree);
+    property_set("dalvik.vm.heapmaxfree", heapmaxfree);
 
-  property_set("qemu.hw.mainkeys", "0");
+    property_set("ro.hwui.texture_cache_size", "72");
+    property_set("ro.hwui.layer_cache_size", "48");
+    property_set("ro.hwui.r_buffer_cache_size", "8");
+    property_set("ro.hwui.path_cache_size", "32");
+    property_set("ro.hwui.gradient_cache_size", "1");
+    property_set("ro.hwui.drop_shadow_cache_size", "6");
+    property_set("ro.hwui.texture_cache_flushrate", "0.4");
+    property_set("ro.hwui.text_small_cache_width", "1024");
+    property_set("ro.hwui.text_small_cache_height", "1024");
+    property_set("ro.hwui.text_large_cache_width", "2048");
+    property_set("ro.hwui.text_large_cache_height", large_cache_height);
 
-  std::string cmv = property_get("ro.boot.cmv");
+    property_set("qemu.hw.mainkeys", "0");
 
-  if (cmv == "mv1") {
-    /* Swift 2 */
-    property_set("ro.sf.lcd_density", "320");
-    property_set("ro.media.maxmem", "10590068224");
-  } else if (cmv == "mv2") {
-    /* Swift 2 Plus*/
-    property_set("ro.sf.lcd_density", "320");
-  } else if (cmv == "mv3") {
-    /* Swift 2 X */
-    property_set("ro.sf.lcd_density", "440");
-    property_set("persist.bootanimation.scale", "1.5");
-    property_set("sys.lights.capabilities", "3");
-  }
+    check_aw87319();
 
-  init_alarm_boot_properties();
+    std::string cmv = property_get("ro.boot.cmv");
+
+    if (cmv == "mv1")
+    {
+        /* Swift 2 */
+        property_set("ro.sf.lcd_density", "320");
+        property_set("ro.media.maxmem", "10590068224");
+    }
+    else if (cmv == "mv2")
+    {
+        /* Swift 2 Plus*/
+        property_set("ro.sf.lcd_density", "320");
+    }
+    else if (cmv == "mv3")
+    {
+        /* Swift 2 X */
+        property_set("ro.sf.lcd_density", "440");
+        property_set("persist.bootanimation.scale", "1.5");
+        property_set("sys.lights.capabilities", "3");
+    }
 }
 
