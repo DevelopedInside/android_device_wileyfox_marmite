536516e178450ede7c22810d220413e3ec3fb0b5
BeYkeRYkt <beykerykt@gmail.com>
2018-01-15 01:54:14 +0900

    marmite: rebase libinit
    * based on msm9016-common
    * move hwui to libinit
    * formatted
    
    Change-Id: Ieb82971928dd07a7c5270472fbfa1974304134c8

--- product/display.mk
+++ product/display.mk
@@ -4,5 +4,3 @@ TARGET_SCREEN_WIDTH := 720
 
 PRODUCT_AAPT_CONFIG := normal xhdpi xxhdpi
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
-
-$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
