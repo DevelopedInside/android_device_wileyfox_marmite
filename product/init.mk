0bfe960fd51b16aed69cc3b2a82949da44c2d93e
BeYkeRYkt <beykerykt@gmail.com>
2018-01-15 01:31:02 +0900

    marmite: product: clean up
    Change-Id: I5464e74ab4af066c033c08835263f8843003cc15

--- product/init.mk
+++ product/init.mk
@@ -1,4 +1,4 @@
-# Init scripts
+# Root init scripts
 PRODUCT_PACKAGES += \
     fstab.qcom \
     init.class_main.sh \
@@ -11,10 +11,16 @@ PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
     init.qcom.usb.sh \
     init.target.rc \
-    init.variant.mv1.rc \
-    init.variant.mv2.rc \
-    init.variant.mv3.rc \
-    ueventd.qcom.rc \
+    ueventd.qcom.rc
+
+# etc scripts
+PRODUCT_PACKAGES += \
     init.qcom.bt.sh \
     init.qcom.fm.sh \
     init.qcom.post_boot.sh
+
+# Wileyfox varinats
+PRODUCT_PACKAGES += \
+    init.variant.mv1.rc \
+    init.variant.mv2.rc \
+    init.variant.mv3.rc
--- product/qcom-keystore.mk
+++
@@ -1,5 +0,0 @@
-# Keystore (LineageOS's keystore doesn't support msm8937, we use msm8952)
-ifneq ($(TARGET_PROVIDES_KEYMASTER),true)
-PRODUCT_PACKAGES += \
-    keystore.msm8952
-endif
