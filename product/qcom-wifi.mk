0613318248b1532ffc40fd88919eb3e0afcab27c
jrior001 <jriordan001@gmail.com>
2018-02-14 22:13:17 -0500

    marmite: move wifi configs to vendor
    Change-Id: Id8b27983fa4ecf4835ae2da8b9031e9d962bdb72

--- product/qcom-wifi.mk
+++ product/qcom-wifi.mk
@@ -30,5 +30,5 @@ PRODUCT_PACKAGES += \
     wpa_supplicant.conf
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
-    $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf
+    $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:system/vendor/etc/wifi/p2p_supplicant_overlay.conf \
+    $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:system/vendor/etc/wifi/wpa_supplicant_overlay.conf
