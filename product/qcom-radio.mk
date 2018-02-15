fe1ea513afce328a29b999c090dc41a515951c64
Michael Bestas <mkbestas@lineageos.org>
2018-02-14 21:00:17 -0500

    marmite: Move sec_config to vendor
    Change-Id: Ie5f7c8f96243a4ed354f87d78ede9176a46b44be

--- product/qcom-radio.mk
+++ product/qcom-radio.mk
@@ -1,6 +1,6 @@
 # IPC router config
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
+    $(LOCAL_PATH)/configs/sec_config:system/vendor/etc/sec_config
 
 # RIL
 PRODUCT_PACKAGES += \
