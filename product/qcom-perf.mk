54fc2f87c58250be56f54e293a380f049fa95d99
oshmoun <oshmoun100@googlemail.com>
2018-02-18 23:00:04 +0100

    marmite: Add public.libraries.txt
    Android needs this text file to know what libraries are allowed to be loaded by apps directly.
    For now, add:
    libOpenCL.so to the list, in order to get OpenCL to function properly.
    adsprpc related libraries, since they were also declared public in stock.
    
    Change-Id: I15ef7a7ab7d1f1a0cc6b484cd2ca9993f40de23b

--- product/qcom-perf.mk
+++ product/qcom-perf.mk
@@ -1,3 +1,7 @@
 # Powerhint
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+
+# Public Libraries
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
