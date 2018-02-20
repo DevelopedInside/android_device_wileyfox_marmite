8ea273d848b8f878cd4081d7b8ec8f9c80229b5a
dianlujitao <dianlujitao@lineageos.org>
2018-02-20 22:47:08 +0800

    marmite: Build LiveDisplay HAL
    Change-Id: I4bfac85c64599ced45983a86d98cfd810a97db99

---
+++ product/lineage_livedisplay.mk
@@ -0,0 +1,7 @@
+# Display calibration
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@1.0-service-sdm \
+    vendor.lineage.livedisplay-V1.0-java
+
+PRODUCT_BOOT_JARS += \
+    vendor.lineage.livedisplay-V1.0-java
