75a2d49fda5d7c3fc8e72b84cb020bdc5cb98b78
Tom Cherry <tomcherry@google.com>
2018-03-01 17:00:20 -0800

    marmite: init: Remove ro.hwui.* properties
    These have migrated away from properties.
    
    Bug: 73905119
    Test: n/a
    Change-Id: I83e0942c027defba02a037f9db684d42f5042bb9
    We are not calling the make files that are being removed from
    frameworks/native, but we might as well remove these props.
    Signed-off-by: Tim Clark <tim.clark.82@gmail.com>
    https://android.googlesource.com/platform/frameworks/base/+/642ebea6e14
    Signed-off-by: D. Andrei Măceș <andrei@unlegacy-android.org>

--- init/init_marmite.cpp
+++ init/init_marmite.cpp
@@ -56,7 +56,6 @@ char const* heapgrowthlimit;
 char const* heapsize;
 char const* heapminfree;
 char const* heapmaxfree;
-char const* large_cache_height;
 
 static void init_alarm_boot_properties()
 {
@@ -106,7 +105,6 @@ void check_device()
         heapsize = "768m";
         heapminfree = "512k";
         heapmaxfree = "8m";
-        large_cache_height = "1024";
     }
     else
     {
@@ -116,7 +114,6 @@ void check_device()
         heapsize = "512m";
         heapminfree = "2m";
         heapmaxfree = "8m";
-        large_cache_height = "1024";
     }
 }
 
@@ -163,18 +160,6 @@ void vendor_load_properties()
     property_set("dalvik.vm.heaptargetutilization", "0.75");
     property_set("dalvik.vm.heapminfree", heapminfree);
     property_set("dalvik.vm.heapmaxfree", heapmaxfree);
-
-    property_set("ro.hwui.texture_cache_size", "72");
-    property_set("ro.hwui.layer_cache_size", "48");
-    property_set("ro.hwui.r_buffer_cache_size", "8");
-    property_set("ro.hwui.path_cache_size", "32");
-    property_set("ro.hwui.gradient_cache_size", "1");
-    property_set("ro.hwui.drop_shadow_cache_size", "6");
-    property_set("ro.hwui.texture_cache_flushrate", "0.4");
-    property_set("ro.hwui.text_small_cache_width", "1024");
-    property_set("ro.hwui.text_small_cache_height", "1024");
-    property_set("ro.hwui.text_large_cache_width", "2048");
-    property_set("ro.hwui.text_large_cache_height", large_cache_height);
 
     check_aw87319();
 
