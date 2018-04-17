81908b119d6d2ed7574a6e116d1206cfce6cfd43
Vasya Borisov <vasy@vasy.ru>
2018-04-17 15:30:31 +0300

    Revert "marmite: remove usb otg"
    This reverts commit 50aa83e305c0d3f2ca64c2d33d396cb2e79e5238.

---
+++ product/usb.mk
@@ -0,0 +1,4 @@
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.ethernet.xml:system/vendor/etc/permissions/android.hardware.ethernet.xml \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/vendor/etc/permissions/android.hardware.usb.accessory.xml
