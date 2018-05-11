08f73335d3187a4d537be18f43ed93d29f1f3a0d
BeYkeRYkt <beykerykt@gmail.com>
2018-05-11 18:03:59 +0900

    marmite: use autodetect SDClang
    * use Snapdragon LLVM, if available:
    https://github.com/LineageOS/android_vendor_cm/commit/26e669badc0154baf8fd4b7e2da84fea1d91249a
    
    Change-Id: Iaafb2784680ddf7b344409372f1751937e72b702

--- board/qcom-sdclang.mk
+++ board/qcom-sdclang.mk
@@ -1,11 +1,2 @@
 # Use Snapdragon LLVM, if available
-TARGET_USE_SDCLANG := false
-
-# SDClang 3.8.8
-ifeq ($(TARGET_USE_SDCLANG),true)
-ifneq ($(HOST_OS),darwin)
-SDCLANG := true
-SDCLANG_PATH := prebuilts/clang/linux-x86/host/sdclang-3.8.8/bin
-SDCLANG_LTO_DEFS := device/qcom/common/sdllvm-lto-defs.mk
-endif
-endif
+TARGET_USE_SDCLANG := true
