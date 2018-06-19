9ba2d8d7d1c245930b6a5a02c536ddb530408c46
BeYkeRYkt <beykerykt@gmail.com>
2018-06-19 21:38:52 +0900

    marmite: update perf configs paths
    * Wileyfox/Swift2/marmite:8.1.0/OPM5.171019.017/38c792b31d
    
    Change-Id: I027221686b780aaa9ca62b4c204d5617ff3e18d1

--- product/perf.mk
+++ product/perf.mk
@@ -1,4 +1,4 @@
 # Perf
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/perf/perf-profile0.conf:system/vendor/etc/perf-profile0.conf \
-    $(LOCAL_PATH)/configs/perf/perf-profile1.conf:system/vendor/etc/perf-profile1.conf
+    $(LOCAL_PATH)/configs/perf/perf-profile0.conf:system/vendor/etc/perf/perf-profile0.conf \
+    $(LOCAL_PATH)/configs/perf/perf-profile1.conf:system/vendor/etc/perf/perf-profile1.conf
