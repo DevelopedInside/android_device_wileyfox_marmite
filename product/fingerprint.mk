64de811999add00e3239d19be3a0e774f16236a7
vm03 <vasy@vasy.ru>
2018-07-28 20:20:53 +0300

    Revert "marmite: Build Biometrics HIDL HAL interface"
    This reverts commit 02be409498e34def7e4fd1956355db608145de4d.

--- product/fingerprint.mk
+++ product/fingerprint.mk
@@ -1,7 +1,3 @@
-# Fingerprint
-PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service
-
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml
