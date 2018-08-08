eb266d9a523ac9f1cb266dc36202f176889ddfaa
BeYkeRYkt <beykerykt@gmail.com>
2018-08-08 16:20:09 +0900

    marmite: move configs to vendor
    * We do not make any changes to these files. Return them to blob list.
    
    Change-Id: Id62ff3cf55b70b8579d59c9f0590f4177e6f5de0

--- product/perf.mk
+++
@@ -1,4 +0,0 @@
-# Perf
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/perf/perf-profile0.conf:system/vendor/etc/perf/perf-profile0.conf \
-    $(LOCAL_PATH)/configs/perf/perf-profile1.conf:system/vendor/etc/perf/perf-profile1.conf
--- product/qcom-camera.mk
+++ product/qcom-camera.mk
@@ -9,18 +9,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
 
-# Camera configs
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/camera/camera_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/camera_config.xml \
-    $(LOCAL_PATH)/configs/camera/csidtg_camera.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/csidtg_camera.xml \
-    $(LOCAL_PATH)/configs/camera/csidtg_chromatix.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/csidtg_chromatix.xml \
-    $(LOCAL_PATH)/configs/camera/ov8856_chromatix.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/ov8856_chromatix.xml \
-    $(LOCAL_PATH)/configs/camera/ov8865_chromatix.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/ov8865_chromatix.xml \
-    $(LOCAL_PATH)/configs/camera/s5k3l8_chromatix.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/s5k3l8_chromatix.xml \
-    $(LOCAL_PATH)/configs/camera/s5k3l8_f3l8yam_chromatix.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/s5k3l8_f3l8yam_chromatix.xml \
-    $(LOCAL_PATH)/configs/camera/s5k3p3_chromatix.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/s5k3p3_chromatix.xml \
-    $(LOCAL_PATH)/configs/camera/s5k3p3_x50l_chromatix.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/s5k3p3_x50l_chromatix.xml
-
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
