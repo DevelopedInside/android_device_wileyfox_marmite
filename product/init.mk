b7486d50dedacee1ffe7008b851040d84787cab8
BeYkeRYkt <beykerykt@gmail.com>
2018-08-11 00:04:53 +0900

    [DO NOT MERGE] marmite: restore init.qcom.power.rc
    * with updated power params from CAF
    
    Change-Id: I9140fa93e8d329ee87984f020f80843db5103159

--- product/init.mk
+++ product/init.mk
@@ -36,6 +36,7 @@ PRODUCT_PACKAGES += \
     init.msm.usb.configfs.rc \
     init.qcom.factory.rc \
     init.qcom.rc \
+    init.qcom.power.rc \
     init.qcom.usb.rc \
     init.target.rc \
 
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -192,6 +192,14 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qcom.power.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := vendor/etc/init/hw/init.qcom.power.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
