c3901076e9c70d2bb0fd51a8a49682321afd5238
BeYkeRYkt <beykerykt@gmail.com>
2018-08-15 00:09:52 +0900

    marmite: move thermal configs to vendor
    Change-Id: I33912400e5f99c092a60bd8dab469078d56d078e

--- product/qcom-thermal.mk
+++ product/qcom-thermal.mk
@@ -1,4 +1,4 @@
 # Thermal
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal-engine.conf:system/etc/thermal-engine.conf \
-    $(LOCAL_PATH)/configs/thermal-engine.x50l.conf:system/etc/thermal-engine.x50l.conf
+    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf \
+    $(LOCAL_PATH)/configs/thermal-engine.x50l.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.x50l.conf
