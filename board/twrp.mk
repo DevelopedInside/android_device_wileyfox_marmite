bf6bf57b79b029732a1f8010bbc1a734ae5e9385
vm03 <vasy@vasy.ru>
2018-10-19 16:20:46 +0300

    marmite: twrp: add TWRP support

---
+++ board/twrp.mk
@@ -0,0 +1,16 @@
+ifeq ($(WITH_TWRP),true)
+RECOVERY_VARIANT := twrp
+TW_THEME := portrait_hdpi
+RECOVERY_SDCARD_ON_DATA := true
+#TW_TARGET_USES_QCOM_BSP := true
+TW_INCLUDE_CRYPTO := true
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+TARGET_RECOVERY_DEVICE_DIRS += $(DEVICE_PATH)/twrp
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+TW_CUSTOM_CPU_TEMP_PATH := /sys/class/thermal/thermal_zone4/temp
+TW_EXCLUDE_SUPERSU := true
+TW_EXCLUDE_TWRPAPP := true
+TW_USE_TOOLBOX := true
+endif
