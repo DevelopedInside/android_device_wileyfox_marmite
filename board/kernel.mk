46b0b6b4fa69eeec5963742f25e91bfd05abf71f
Nik <nikith290@gmail.com>
2018-11-07 16:31:18 +0700

    marmite: SELinux: permissive

--- board/kernel.mk
+++ board/kernel.mk
@@ -1,5 +1,6 @@
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78B0000
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
 
