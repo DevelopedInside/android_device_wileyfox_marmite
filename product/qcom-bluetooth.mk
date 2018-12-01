388fd7244bfccd83109fc70b2146b8b68330c79a
Vitaliy Tomin <highwaystar.ru@gmail.com>
2018-12-01 23:11:35 +0800

    marmite: Cleanup bluetooth blobs
    * use oss android.hardware.bluetooth@1.0-service and proprietary
    android.hardware.bluetooth@1.0-impl-qti.so

--- product/qcom-bluetooth.mk
+++ product/qcom-bluetooth.mk
@@ -2,6 +2,9 @@
 PRODUCT_PACKAGES += \
     libbt-vendor
 
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0-service
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:system/vendor/etc/permissions/android.hardware.bluetooth.xml \
