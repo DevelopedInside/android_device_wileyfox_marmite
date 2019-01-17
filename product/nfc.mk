c4e5ba46e588fb7c7bd07dced290cc6c2244608a
vm03 <vasy@vasy.ru>
2019-01-17 14:44:07 +0300

    Revert "marmite: drop NFC for some time"
    This reverts commit 5f475949ec4518626bc2af5c4630fd4f816cc8c9.

--- product/nfc.mk
+++ product/nfc.mk
@@ -1,20 +1,20 @@
 # NFC
-#PRODUCT_PACKAGES += \
- #   android.hardware.nfc@1.0-impl \
- #   android.hardware.nfc@1.0-service \
- #   com.android.nfc_extras \
- #   nfc_nci.msm8937 \
- #   NfcNci \
- #   Tag
+PRODUCT_PACKAGES += \
+    android.hardware.nfc@1.0-impl \
+    android.hardware.nfc@1.0-service \
+    com.android.nfc_extras \
+    nfc_nci.msm8937 \
+    NfcNci \
+    Tag
 
 # NFC
-#PRODUCT_COPY_FILES += \
-   # $(LOCAL_PATH)/configs/nfc/libnfc-brcm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-brcm.conf \
-   # $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
-   
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/nfc/libnfc-brcm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-brcm.conf \
+    $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
+
 # Permissions
-#PRODUCT_COPY_FILES += \
-   # frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
-   # frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
-   # frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
-   # frameworks/native/data/etc/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
+    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
+    frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
+    frameworks/native/data/etc/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml
