abbde5d23c049c315702140b00f93fbca784502a
Nich <nctrenco@gmail.com>
2019-04-21 15:00:47 +0800

    marmite: camera: Disable CDS mode on k4.9
    Turning on CDS causes green artifacts in taken pictures. CAF's
    workaround [1] was to disable CDS by default, but it doesn't prevent an
    app from enabling it.
    
    [1]: https://source.codeaurora.org/quic/la/platform/hardware/qcom/camera/commit/?id=9eeafe30749482b8dbb14cbaeda6df59c5ec7b91
    
    Change-Id: If22f87537c942adcf9b40bdbe3204de032fcb2d8

--- camera/QCamera2/HAL3/QCamera3HWI.cpp
+++ camera/QCamera2/HAL3/QCamera3HWI.cpp
@@ -10315,6 +10315,7 @@ int QCamera3HardwareInterface::translateToHalMetadata
         }
     }
 
+#ifndef NO_CDS_MODE
     // CDS for non-HFR non-video mode
     if ((mOpMode != CAMERA3_STREAM_CONFIGURATION_CONSTRAINED_HIGH_SPEED_MODE) &&
             !(m_bIsVideo) && frame_settings.exists(QCAMERA3_CDS_MODE)) {
@@ -10328,6 +10329,7 @@ int QCamera3HardwareInterface::translateToHalMetadata
             }
         }
     }
+#endif
 
     // TNR
     if (frame_settings.exists(QCAMERA3_TEMPORAL_DENOISE_ENABLE) &&
