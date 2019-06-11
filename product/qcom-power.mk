228c28ed1cba387c4df50c84e6d40c0373c9c482
BeYkeRYkt <beykerykt@gmail.com>
2019-06-11 17:35:41 +0900

    marmite: restore lineage power hal
    Change-Id: I9e59484f9e3b8e01df231e3cd34e97f26d7f7ac3

--- product/qcom-power.mk
+++ product/qcom-power.mk
@@ -8,9 +8,7 @@ PRODUCT_COPY_FILES += \
 
 # Power HAL
 PRODUCT_PACKAGES += \
-    power.msm8937 \
-    android.hardware.power@1.0-impl \
-    android.hardware.power@1.0-service
+    android.hardware.power@1.1-service-qti
 
 # Powerhint
 PRODUCT_COPY_FILES += \
