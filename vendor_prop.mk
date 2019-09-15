ec5589878213e84d995ba2bdfbabb03bf61b77a4
Nikita <nikith290@gmail.com>
2019-09-15 21:22:34 +0700

    marmite: update USB props

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -228,7 +228,15 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # USB
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.usb.vid=05C6
+    ro.usb.id.midi=90BA \
+    ro.usb.id.midi_adb=90BB \
+    ro.usb.id.mtp=2281 \
+    ro.usb.id.mtp_adb=2282 \
+    ro.usb.id.ptp=2284 \
+    ro.usb.id.ptp_adb=2283 \
+    ro.usb.id.ums=2286 \
+    ro.usb.id.ums_adb=2285 \
+    ro.usb.vid=2970 
 
 # Time services
 PRODUCT_PROPERTY_OVERRIDES += \
