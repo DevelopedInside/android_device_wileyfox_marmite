3b031dad04a473b6ec45cc1a779c5cee4ee1ff66
Bruno Martins <bgcngm@gmail.com>
2019-09-22 14:49:07 +0100

    marmite: Switch to cryptfshw HIDL
    Change-Id: Id1d580607a7d73ec6738ab3aa8dcdf9e27558a33

--- device.mk
+++ device.mk
@@ -207,6 +207,10 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.0-impl \
     android.hardware.health@2.0-service
 
+# HW crypto
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
+
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
