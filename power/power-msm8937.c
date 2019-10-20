5e8581edac0f98fddea55fe07a926ba3d35a3cc2
BeYkeRYkt <beykerykt@gmail.com>
2019-10-21 05:11:36 +0900

    marmite: power: return back kMinFlingDuration
    * POWER_HINT_INTERACTION is called during any user interactions with the system,
      and we boost cpu to provide the required level of performance. However, in our
      case, this also increases the heat release in long interactive tasks, which
      causes throttling cpu. Return kMinFlingDuration and boost cpu if duration value
      longer than 1500 milliseconds.
    * Minimum cluster frequencies should be enough for short interactions.
    
    Change-Id: Ie97772d89acddc49f34dd2a32493b7b4a65d6659

--- power/power-msm8937.c
+++ power/power-msm8937.c
@@ -53,6 +53,7 @@
 const int kMaxLaunchDuration = 5000; /* ms */
 const int kMaxInteractiveDuration = 5000; /* ms */
 const int kMinInteractiveDuration = 500; /* ms */
+const int kMinFlingDuration = 1500; /* ms */
 
 static int video_encode_hint_sent;
 
@@ -251,7 +252,9 @@ static void process_interaction_hint(void *data)
     s_previous_boost_timespec = cur_boost_timespec;
     s_previous_duration = duration;
 
-    perf_hint_enable_with_type(VENDOR_HINT_SCROLL_BOOST, duration, SCROLL_VERTICAL);
+    if (duration >= kMinFlingDuration) {
+        perf_hint_enable_with_type(VENDOR_HINT_SCROLL_BOOST, duration, SCROLL_VERTICAL);
+    }
 }
 
 int power_hint_override(power_hint_t hint, void *data)
