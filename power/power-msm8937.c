586c956df9e230165058f4fbf368d50af787aff8
BeYkeRYkt <beykerykt@gmail.com>
2019-11-04 03:41:24 +0700

    marmite: power: do not handle POWER_HINT_VIDEO_DECODE
    Change-Id: I8d7b9b66e7da7cbc91ef63087214e6a5f3a4360e

--- power/power-msm8937.c
+++ power/power-msm8937.c
@@ -286,6 +286,9 @@ int power_hint_override(power_hint_t hint, void *data)
         case POWER_HINT_VIDEO_ENCODE:
             process_video_encode_hint(data);
             return HINT_HANDLED;
+        case POWER_HINT_VIDEO_DECODE:
+            // do nothing
+            return HINT_HANDLED;
         case POWER_HINT_INTERACTION:
             process_interaction_hint(data);
             return HINT_HANDLED;
