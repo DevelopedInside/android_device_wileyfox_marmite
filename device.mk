8659fbe40b8e555351270a0d6e3ac6e34ea9083e
BeYkeRYkt <beykerykt@gmail.com>
2019-11-04 18:36:50 +0900

    marmite: move thermal-engine.x50l.conf to vendor
    * Requires following hex edit to load config from vendor:
    sed -i "s|/system/etc/|/vendor/etc/|g" vendor/bin/thermal-engine
    
    Change-Id: I082d1080a0b8aac93fb15abf3daf8c1e419b65bd

--- device.mk
+++ device.mk
@@ -427,7 +427,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf \
-    $(LOCAL_PATH)/configs/thermal-engine.x50l.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/thermal-engine.x50l.conf
+    $(LOCAL_PATH)/configs/thermal-engine.x50l.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.x50l.conf
 
 # Touchscreen
 PRODUCT_COPY_FILES += \
