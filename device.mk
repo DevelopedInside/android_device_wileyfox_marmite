9d95264b4f100b0032e453dfc2237fb4aaa76b50
Mikhail Naganov <mnaganov@google.com>
2019-11-22 10:56:29 -0800

    marmite: Switch to Audio HAL V6
    Switch to Audio HAL V6
    
    Bug: 141989952
    Test: atest VtsHalAudioV6_0TargetTest
          atest VtsHalAudioEffectV6_0TargetTest
                manual audio "smoke" test on taimen and walleye
    
    Change-Id: Ieb1cc5c7fdcc5416a328a0a5c4a1996258b81ac8
    Signed-off-by: Alex Damaratski <alexeydomoratsky1@gmail.com>

--- device.mk
+++ device.mk
@@ -60,8 +60,8 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
-    android.hardware.audio@5.0-impl \
-    android.hardware.audio.effect@5.0-impl \
+    android.hardware.audio@6.0-impl \
+    android.hardware.audio.effect@6.0-impl \
     android.hardware.soundtrigger@2.2-impl
 
 PRODUCT_COPY_FILES += \
