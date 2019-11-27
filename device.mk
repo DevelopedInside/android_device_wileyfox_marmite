27468b4d9eabc11b9da1177954073b05bf75bda2
BeYkeRYkt <beykerykt@gmail.com>
2019-11-28 01:45:46 +0700

    marmite: configs: drop MP-CTL powerhint
    * Unnecessary on this platform
    Change-Id: Idd302a063914ddd1a62916b5cfe3f517cbc3de04

--- device.mk
+++ device.mk
@@ -322,8 +322,7 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.1-service-qti
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/powerhint.xml \
-    $(LOCAL_PATH)/configs/powerhint_mpctl.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/powerhint.xml
 
 # QCOM
 PRODUCT_COPY_FILES += \
