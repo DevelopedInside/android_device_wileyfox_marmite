c1f873a9d0ca51fdfb673a22db23ad1cf1699942
BeYkeRYkt <beykerykt@gmail.com>
2019-12-03 20:15:40 +0700

    marmite: add missing vulkan permission
    Change-Id: I60e32239317d8a7b5bc782f8fe8aee778cffbfdb

--- device.mk
+++ device.mk
@@ -448,6 +448,7 @@ PRODUCT_PACKAGES += \
 
 # Vulkan
 PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.vulkan.compute-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.compute-0.xml \
     frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level-0.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version-1_1.xml
 
