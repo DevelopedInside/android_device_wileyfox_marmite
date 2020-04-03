faa20d56c247c3d8caa8723262bb5a96bd6f69d0
Nik <nikith290@gmail.com>
2020-04-04 00:02:54 +0700

    marmtie: update props

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -28,41 +28,40 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
-    af.fast_track_multiplier=1 \
-    vendor.audio_hal.period_size=192 \
-    ro.vendor.audio.sdk.fluencetype=fluence \
-    persist.vendor.audio.fluence.voicecall=true \
-    persist.vendor.audio.fluence.voicerec=false \
-    persist.vendor.audio.fluence.speaker=true \
-    vendor.audio.tunnel.encode=false \
-    vendor.audio.offload.buffer.size.kb=64 \
-    audio.offload.min.duration.secs=30 \
-    audio.offload.video=true \
-    vendor.audio.offload.track.enable=true \
-    audio.deep_buffer.media=true \
-    vendor.voice.path.for.pcm.voip=true \
-    vendor.audio.offload.multiaac.enable=true \
-    vendor.audio.dolby.ds2.enabled=false \
-    vendor.audio.dolby.ds2.hardbypass=false \
-    vendor.audio.offload.multiple.enabled=false \
-    vendor.audio.offload.passthrough=false \
-    ro.vendor.audio.sdk.ssr=false \
-    vendor.audio.offload.gapless.enabled=true \
-    vendor.audio.safx.pbe.enabled=true \
-    vendor.audio.parser.ip.buffer.size=262144 \
-    vendor.audio.playback.mch.downsample=true \
-    vendor.audio.use.sw.alac.decoder=true \
-    vendor.audio.use.sw.ape.decoder=true \
-    vendor.audio.pp.asphere.enabled=false \
-    vendor.voice.playback.conc.disabled=true \
-    vendor.voice.record.conc.disabled=false \
-    vendor.voice.voip.conc.disabled=true \
-    vendor.voice.conc.fallbackpath=deep-buffer \
-    persist.vendor.audio.speaker.prot.enable=false \
-    vendor.audio.hw.aac.encoder=true \
-    vendor.audio.flac.sw.decoder.24bit=true \
-    ro.af.client_heap_size_kbyte=7168 \
-    persist.vendor.audio.hw.binder.size_kbyte=1024
+af.fast_track_multiplier=2 \
+audio.deep_buffer.media=true \
+audio.offload.disable=true \
+audio.offload.min.duration.secs=30 \
+audio.offload.video=true \
+persist.audio.dirac.speaker=true \
+persist.vendor.audio.fluence.speaker=true \
+persist.vendor.audio.fluence.voicecall=true \
+persist.vendor.audio.fluence.voicerec=false \
+persist.vendor.audio.hw.binder.size_kbyte=1024 \
+persist.vendor.btstack.enable.splita2dp=false \
+ro.config.media_vol_steps=25 \
+ro.config.vc_call_vol_steps=7 \
+ro.vendor.audio.sdk.fluencetype=fluence \
+ro.vendor.audio.sdk.ssr=false \
+vendor.audio.flac.sw.decoder.24bit=true \
+vendor.audio.offload.buffer.size.kb=64 \
+vendor.audio.offload.gapless.enabled=true \
+vendor.audio.offload.multiaac.enable=true \
+vendor.audio.offload.multiple.enabled=false \
+vendor.audio.offload.track.enable=true \
+vendor.audio.parser.ip.buffer.size=0 \
+vendor.audio.playback.mch.downsample=true \
+vendor.audio.pp.asphere.enabled=false \
+vendor.audio.safx.pbe.enabled=true \
+vendor.audio.tunnel.encode=false \
+vendor.audio.use.sw.alac.decoder=true \
+vendor.audio.use.sw.ape.decoder=true \
+vendor.audio_hal.period_size=192 \
+vendor.voice.conc.fallbackpath=deep-buffer \
+vendor.voice.path.for.pcm.voip=true \
+vendor.voice.playback.conc.disabled=true \
+vendor.voice.record.conc.disabled=false \
+vendor.voice.voip.conc.disabled=true
 
 # Battery
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -71,8 +70,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
     bluetooth.hfp.client=1 \
-    persist.vendor.btstack.enable.splita2dp=false \
-    vendor.qcom.bluetooth.soc=smd
+v   endor.qcom.bluetooth.soc=smd \
+r   o.bluetooth.hfp.ver=1.7
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -120,29 +119,30 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
-    debug.egl.hw=0 \
-    debug.enable.sglscale=1 \
-    debug.gralloc.enable_fb_ubwc=1 \
-    debug.mdpcomp.logs=0 \
-    debug.sf.enable_hwc_vds=1 \
-    debug.sf.hw=0 \
-    debug.sf.latch_unsignaled=1 \
-    debug.sf.recomputecrop=0 \
-    dev.pm.dyn_samplingrate=1 \
-    persist.debug.wfd.enable=1 \
-    persist.demo.hdmirotationlock=false \
-    persist.hwc.enable_vds=1 \
-    persist.hwc.mdpcomp.enable=true \
-    ro.opengles.version=196610 \
-    sdm.debug.disable_skip_validate=1 \
-    vendor.display.disable_skip_validate=1 \
-    vendor.display.enable_default_color_mode=1 \
-    vendor.gralloc.enable_fb_ubwc=1 \
-    vendor.opengles.version=196610
+debug.egl.hw=0 \
+debug.enable.sglscale=1 \
+debug.gralloc.enable_fb_ubwc=1 \
+debug.mdpcomp.logs=0 \
+debug.sf.enable_hwc_vds=1 \
+debug.sf.hw=0 \
+debug.sf.latch_unsignaled=1 \
+debug.sf.recomputecrop=0 \
+debug.sf.enable_gl_backpressure=1 \
+dev.pm.dyn_samplingrate=1 \
+persist.demo.hdmirotationlock=false \
+persist.hwc.enable_vds=1 \
+persist.hwc.mdpcomp.enable=true \
+ro.opengles.version=196610 \
+ro.qualcomm.cabl=0 \
+ro.vendor.display.cabl=2 \
+sdm.debug.disable_skip_validate=1 \
+vendor.display.enable_default_color_mode=1 \
+vendor.display.disable_skip_validate=1 \
+vendor.gralloc.enable_fb_ubwc=1
 
 # DRM
 PRODUCT_PROPERTY_OVERRIDES += \
-    drm.service.enabled=1
+    drm.service.enabled=true
 
 # Factory reset partition
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -162,20 +162,38 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.loc.nlp_name=com.qualcomm.location \
     ro.gps.agps_provider=1
 
+# LMKD
+PRODUCT_PRODUCT_PROPERTIES += \
+ro.lmk.low=1001 \
+ro.lmk.medium=800 \
+ro.lmk.critical=0 \
+ro.lmk.critical_upgrade=false \
+ro.lmk.upgrade_pressure=100 \
+ro.lmk.downgrade_pressure=100 \
+ro.lmk.kill_heaviest_task=true \
+ro.lmk.kill_timeout_ms=100 \
+ro.lmk.use_minfree_levels=true
+
+
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
-    media.aac_51_output_enabled=true \
-    media.msm8956hw=0 \
-    media.stagefright.audio.sink=280 \
-    mm.enable.smoothstreaming=true \
-    mmp.enable.3g2=true \
-    persist.mm.sta.enable=0 \
-    vendor.mm.enable.qcom_parser=1048575 \
-    vendor.vidc.disable.split.mode=1 \
-    vendor.vidc.enc.narrow.searchrange=1 \
-    vendor.video.disable.ubwc=1 \
-    mm.enable.qcom_parser=917503 \
-    video.disable.ubwc=1
+av.debug.disable.pers.cache=1 \
+debug.media.codec2=2 \
+debug.stagefright.omx_default_rank.sw-audio=1 \
+debug.stagefright.omx_default_rank=0 \
+media.msm8956hw=0 \
+media.stagefright.audio.sink=280 \
+media.stagefright.thumbnail.prefer_hw_codecs=true \
+mmp.enable.3g2=true \
+vendor.audio.hw.aac.encoder=true \
+vendor.mm.enable.qcom_parser=1048575 \
+vendor.vidc.dec.downscalar_height=1088 \
+vendor.vidc.dec.downscalar_width=1920 \
+vendor.vidc.disable.split.mode=1 \
+vendor.vidc.enc.disable.pq=true \
+vendor.vidc.enc.disable_bframes=1 \
+vendor.video.disable.ubwc=1
+
 
 # Misc
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -205,23 +223,31 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.qti.am.reschedule_service=true \
     ro.vendor.qti.config.zram=true
 
+# Qualcomm
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.vendor.qti.va_aosp.support=1 \
+ro.vendor.qti.va_odm.support=1
+
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
-    DEVICE_PROVISIONED=1 \
-    persist.radio.aosp_usr_pref_sel=true \
-    persist.radio.multisim.config=dsds \
-    persist.vendor.radio.apm_sim_not_pwdn=1 \
-    persist.vendor.radio.custom_ecc=1 \
-    persist.vendor.radio.flexmap_type=dds \
-    persist.vendor.radio.rat_on=combine \
-    persist.vendor.radio.sib16_support=1 \
-    vendor.rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
-    ril.subscription.types=NV,RUIM \
-    rild.libargs=-d/dev/smd0 \
-    rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
-    ro.telephony.call_ring.multiple=false \
-    ro.telephony.default_network=9,9 \
-    telephony.lteOnCdmaDevice=1
+DEVICE_PROVISIONED=1 \
+persist.dbg.volte_avail_ovr=1 \
+persist.dbg.vt_avail_ovr=1 \
+persist.dbg.wfc_avail_ovr=1 \
+persist.radio.multisim.config=dsds \
+persist.sys.fflag.override.settings_network_and_internet_v2=true \
+persist.vendor.radio.apm_sim_not_pwdn=1 \
+persist.vendor.radio.custom_ecc=1 \
+persist.vendor.radio.hw_mbn_update=0 \
+persist.vendor.radio.rat_on=combine \
+persist.vendor.radio.sib16_support=1 \
+persist.vendor.radio.aosp_usr_pref_sel=true \
+ril.subscription.types=NV,RUIM \
+rild.libargs=-d/dev/smd0 \
+rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
+ro.telephony.call_ring.multiple=false \
+ro.telephony.default_network=22,22 \
+service.qti.ims.enabled=1
 
 # Shutdown timeout
 PRODUCT_PROPERTY_OVERRIDES += \
