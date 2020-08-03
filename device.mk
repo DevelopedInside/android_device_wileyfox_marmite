e1fcba98bdbe24bea8fbc14d38c0f935a8d859d1
Nikita <nikith290@gmail.com>
2020-08-03 13:59:06 +0700

    marmite: Make fingerprint work again
    * Remove Biometrics HAL. Use the precompiled Biometrics HAL from our source
      instead, with patchelf replacing libbinder.so with libbindergx.so.
    * Replace libbinder.so with custom libbinder in goodix blobs.
    
    Change-Id: I5cbf4a5993392796dc6c0c19b5ea5624941cfac6

--- device.mk
+++ device.mk
@@ -133,7 +133,8 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.0-service-marmite
+    android.hardware.biometrics.fingerprint@2.1-service \
+    libunwind
 
 # FM
 PRODUCT_PACKAGES += \
