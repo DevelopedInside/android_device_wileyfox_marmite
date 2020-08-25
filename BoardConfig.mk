7d1e757d63df8862251e07f86eb4d380a373bace
thp1997 <tirthpatel49@gmail.com>
2020-08-25 10:12:07 +0300

    marmite: Compile HWUI for Better Performance

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -175,6 +175,9 @@ DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 
+# HWUI
+HWUI_COMPILE_FOR_PERF := true
+
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_marmite
 TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
