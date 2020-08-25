f6b9cfc063d721835fb57e5a25fac1b05c488a82
Daniel Sikorski <dadi.sikorski@gmail.com>
2020-08-25 10:07:26 +0300

    dex2oat: use 8 cores instead of 4
    use more cores for faster app installation

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -197,6 +197,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Perf
 PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sys.fw.dex2oat_thread_count=8 \
     ro.vendor.at_library=libqti-at.so \
     ro.vendor.extension_library=libqti-perfd-client.so \
     ro.vendor.gt_library=libqti-gt.so \
