08302be068e5c8c427f22d3a6b7d93d8878e7193
BeYkeRYkt <beykerykt@gmail.com>
2021-01-03 16:33:15 +0900

    marmite: rootdir: Let kernel know our system image
    Change-Id: I9400832e99533b0d82062d1ab16c04a4096b8b13

--- rootdir/vendor/bin/init.qcom.mem.sh
+++ rootdir/vendor/bin/init.qcom.mem.sh
@@ -71,3 +71,19 @@ echo 1024 > /sys/module/process_reclaim/parameters/per_swap_size
 echo "18432,23040,27648,32256,55296,80640" > /sys/module/lowmemorykiller/parameters/minfree
 # For 64-bit arch, vmpressure_file_min = LMK minfree's last bin value
 echo 80640 > /sys/module/lowmemorykiller/parameters/vmpressure_file_min
+
+# Let kernel know our image version/variant/crm_version
+if [ -f /sys/devices/soc0/select_image ]; then
+    image_version="10:"
+    image_version+=`getprop ro.build.id`
+    image_version+=":"
+    image_version+=`getprop ro.build.version.incremental`
+    image_variant=`getprop ro.product.name`
+    image_variant+="-"
+    image_variant+=`getprop ro.build.type`
+    oem_version=`getprop ro.build.version.codename`
+    echo 10 > /sys/devices/soc0/select_image
+    echo $image_version > /sys/devices/soc0/image_version
+    echo $image_variant > /sys/devices/soc0/image_variant
+    echo $oem_version > /sys/devices/soc0/image_crm_version
+fi
