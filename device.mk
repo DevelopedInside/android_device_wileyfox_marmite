ce6db81800d3b6d58183d6feb37cc0e0d397f278
Weilun Du <wdu@google.com>
2021-06-27 15:54:48 +0000

    marmite: Add TetheringOverlay
    BUG: 163800715
    Signed-off-by: Weilun Du <wdu@google.com>
    Change-Id: Ia0834f5215ff8c3b13effa591bd2f84d93419a06
    Signed-off-by: Alex Damaratski <alexeydomoratsky1@gmail.com>

--- device.mk
+++ device.mk
@@ -421,6 +421,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf \
     $(LOCAL_PATH)/configs/thermal-engine.x50l.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.x50l.conf
 
+# Tethering
+PRODUCT_PACKAGES += \
+    TetheringConfigOverlay
+
 # Touch features
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.marmite
