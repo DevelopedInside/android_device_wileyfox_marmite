e663f750bdbd90ce165979024128d27c43fd666f
Alex Damaratski <alexeydomoratsky1@gmail.com>
2021-06-27 14:33:52 +0000

    marmite: Move telephony packages to /product
    Signed-off-by: Alex Damaratski <alexeydomoratsky1@gmail.com>
    Change-Id: Ib3aae2de77455af5c61f31ed94bbd8ba0e77301b

--- extract-files.sh
+++ extract-files.sh
@@ -51,6 +51,14 @@ if [ -z "${SRC}" ]; then
     SRC="adb"
 fi
 
+function blob_fixup() {
+    case "${1}" in
+        product/etc/permissions/qcrilhook.xml)
+            sed -i "s|/system/framework/qcrilhook.jar|/system/product/framework/qcrilhook.jar|g" "${2}"
+            ;;
+    esac
+}
+
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
