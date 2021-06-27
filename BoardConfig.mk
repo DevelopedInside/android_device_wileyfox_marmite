83ba88a21de4870f2556d10ebfe4056b6e4a48fe
Alex Damaratski <alexeydomoratsky1@gmail.com>
2021-06-27 15:48:10 +0000

    marmite: Build new aidl qti power HAL
    Signed-off-by: Alex Damaratski <alexeydomoratsky1@gmail.com>
    Change-Id: I9400554714ca152191afcb14a0add50a466d2f00

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -235,10 +235,6 @@ TARGET_USERIMAGES_USE_F2FS := true
 # Peripheral manager
 TARGET_PER_MGR_ENABLED := true
 
-# Power
-BOARD_POWER_CUSTOM_BOARD_LIB := libpower_msm8937
-TARGET_USES_INTERACTION_BOOST := true
-
 # Qualcomm support
 BOARD_USES_QCOM_HARDWARE := true
 
--- device.mk
+++ device.mk
@@ -316,7 +316,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti
+    android.hardware.power-service-qti
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/powerhint.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/powerhint.xml
