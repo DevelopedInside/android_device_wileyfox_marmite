e7bf0a056d26f018b04b323954022559da4a9685
Alex Damaratski <alexeydomoratsky1@gmail.com>
2021-06-27 16:44:17 +0000

    marmite: Use CodeAurora ImsService implementation for RCS
    There is no evidence that Qualcomm devices running a rather recent IMS
    stack need to include AOSP RCS packages. RCS already worked before this
    change and continues to do so, but the main goal was to get rid of some
    of the RCS related log spam, which was accomplished here.
    
    Signed-off-by: Alex Damaratski <alexeydomoratsky1@gmail.com>
    Change-Id: I54ef5d8b41764705828451e2f06539838d569fde

--- device.mk
+++ device.mk
@@ -349,12 +349,6 @@ PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
     init.target.rc
 
-# RCS
-PRODUCT_PACKAGES += \
-    com.android.ims.rcsmanager \
-    PresencePolling \
-    RcsService
-
 # RenderScript HAL
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl \
